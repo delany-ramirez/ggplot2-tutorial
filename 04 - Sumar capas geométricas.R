e152f88e924ff922a163a2802e167f2a2f189adf
delany-ramirez <delram@utp.edu.co>
2019-08-04 12:59:08 -0500

    Update 04 - Sumar capas geométricas.R

--- 04 - Sumar capas geométricas.R
+++ 04 - Sumar capas geométricas.R
@@ -14,17 +14,26 @@ datos %>%
   geom_point() +
   geom_boxplot()
 
+## Gráfico de Caja con puntos sobrexpuestos
+
 datos %>% 
   ggplot(aes(x = gear, y = mpg)) + 
   geom_boxplot() +
   geom_point() 
+
+## Gráfico de Puntos con Caja transparente sobrexpuesta
 
 datos %>% 
   ggplot(aes(x = gear, y = mpg)) + 
   geom_point() +
   geom_boxplot(alpha = 0.3)
 
+## Cálculo de la media de mpg
+
 media <- mean(datos$mpg)
+
+
+## Gráfico de Caja con puntos sobrexpuestos y línea de media de mpg
 
 datos %>% 
   ggplot(aes(x = gear, y = mpg)) + 
